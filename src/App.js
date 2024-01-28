64364855d443099946e3dfe4279d0c4fa4ffe6ef
Evan Wildenhain <evan.wildenhain@gmail.com>
2024-01-28 12:27:29 -0500

    square coordinates trainer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import { onCleanup, createSignal, createEffect } from "https://esm.sh/solid-js@1.8.12";
+import { createSignal, createEffect, onMount, on } from "https://esm.sh/solid-js@1.8.12";
+import { createStore, produce } from "https://esm.sh/solid-js@1.8.12/store";
 import html from "https://esm.sh/solid-js@1.8.12/html";
 import { Chessboard2 } from "https://unpkg.com/@chrisoakman/chessboard2@0.5.0/dist/chessboard2.min.mjs";
 
 const FILES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const RANKS = [1, 2, 3, 4, 5, 6, 7, 8]
+
+function randInt(n) {
+  return Math.floor(Math.random() * n)
+}
+
+function randSquare() {
+  return `${FILES[randInt(FILES.length)]}${RANKS[randInt(RANKS.length)]}`
+}
+
+// see https://github.com/google/closure-compiler/wiki/Annotating-JavaScript-for-the-Closure-Compiler#type-annotations
+/**
+ * @typedef {{
+ *  position: Function
+ * }}
+ */
+let Chessboard;
 
 export function App() {
-  const [rank, setRank] = createSignal(0);
-  const [file, setFile] = createSignal(0);
-  const [chessboard, setChessboard] = createSignal(null);
-  const rankTimer = setInterval(() => setRank(Math.floor(Math.random() * FILES.length)), 777);
-  const fileTimer = setInterval(() => setFile(Math.floor(Math.random() * FILES.length)), 1337);
-  onCleanup(() => {
-    clearInterval(rankTimer);
-    clearInterval(fileTimer);
+  const [chessboard, setChessboard] = createSignal();
+  const [clickedSquare, setClickedSquare] = createSignal();
+  const [desiredSquare, setDesiredSquare] = createSignal(randSquare(), {
+    equals: false,
   });
-  createEffect(() => {
-    setChessboard(Chessboard2("chessboard"));
+  const [history, setHistory] = createStore([]);
+  onMount(() => {
+    setChessboard(Chessboard2("chessboard", {
+      onMouseupSquare: (event, domEvent) => {
+        setClickedSquare(event.square);
+      }
+    }));
   });
-  createEffect(() => {
-    let board = chessboard();
-    if (!board) {
+  createEffect(on(clickedSquare, sq => {
+    if (!sq) {
       return;
     }
-
-    board.position(
-      {
-        [`${FILES[file()]}${rank() + 1}`]: 'wN'
-      }
-    )
-  });
+    setHistory(produce(history => {
+      history.push(desiredSquare() === sq ? "✅" : "❌");
+    }));
+    setDesiredSquare(randSquare());
+  }));
   return html`
 <div class="board">
-  <p>This is a chessboard with a knight moving around randomly:</p>
+  <p>Click on the following square:</p>
+  <h2>${() => desiredSquare()}</h2>
+  <h3>Record so far: ${() => history.join("")}</h3>
   <div id="chessboard"></div>
 </div>
 
